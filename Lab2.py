7271466dfc678988b04adfc7e06a598c0c8bd31e
GalvinF <78511747+GalvinF@users.noreply.github.com>
2021-06-16 09:50:48 -0700

    Add files via upload

---
+++ Lab2.py
@@ -0,0 +1,19 @@
+name = "LeBron"
+age = 35
+salary = 37.44
+plays_basketball = True
+jersey_number = "23"
+print(name)
+print(age)  
+print (salary)
+print(plays_basketball)
+print(jersey_number)
+car_name = "Subaru"
+x = 50
+two_my_first_name = "John"
+print(car_name)
+print(x)
+print(two_my_first_name)
+type(car_name)
+type(x)
+type(two_my_first_name)
