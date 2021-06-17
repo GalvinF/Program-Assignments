63d295b087b0ef5d36c7e601b92fd04666b8045f
GalvinF <78511747+GalvinF@users.noreply.github.com>
2021-06-17 09:12:15 -0700

    Add files via upload

---
+++ Dictionary_Practice.py
@@ -0,0 +1,26 @@
+groceries = {"Chicken": "$1:59" , "Beef": "$1:99" , "Cheese": "$1:00" , "Milk": "$2.50"}
+movies = {"A Quiet Place": "Thriller" , "Avengers: Endgame": "Action" , "The Martian": "Sci-Fi" , "Dumb and Dumber": "Comedy"}
+
+chicken_price = groceries["Chicken"]
+print(chicken_price)
+
+beef_price = groceries["Beef"]
+print(beef_price)
+
+cheese_price = groceries["Cheese"]
+print(cheese_price)
+
+milk_price = groceries["Milk"]
+print(milk_price)
+
+a_quiet_place_genre = movies["A Quiet Place"]
+print(a_quiet_place_genre)
+
+avengers_endgame = movies["Avengers: Endgame"]
+print(avengers_endgame)
+
+the_martian = movies["The Martian"]
+print(the_martian)
+
+dumb_and_dumber = movies["Dumb and Dumber"]
+print(dumb_and_dumber)
---
+++ NBA.py
@@ -0,0 +1,9 @@
+NBA_players = {"Lebron James": 23 , "Kevin Durant": 35}
+
+NBA_players["Lebron James"] = 6
+jersey_number = NBA_players["Lebron James"]
+print(jersey_number)
+
+NBA_players["Lebron James"] -= 17
+jersey_number = NBA_players["Lebron James"]
+print(jersey_number)
