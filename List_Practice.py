a864ed7de3dc48c03be174210e1cb291a49fdfc7
GalvinF <78511747+GalvinF@users.noreply.github.com>
2021-06-18 12:50:00 -0700

    Add files via upload

---
+++ List_Practice.py
@@ -0,0 +1,25 @@
+citiesList = ["Oakland", "Atlanta", "New York City", "Seattle", "Memphis", "Miami", "Boston", "Los Angeles", "Denver", "New Orleans"]
+print(citiesList)
+
+# print my top 3 cities
+print(citiesList[0])
+print(citiesList[2])
+print(citiesList[7])
+
+# print first three cities in list
+threeCities = citiesList[0:3]
+print(threeCities)
+
+# change list
+citiesList[0]= "San Francisco"
+citiesList[2]= "Brooklyn"
+citiesList[7]= "Hollywood"
+citiesList[4]= "Tampa"
+print(citiesList)
+
+# another practice list
+animalsList = ["dog", "cat", "horse", "giraffe", "elephant", "tiger", "penguin", "gorilla", "gazelle", "panda"]
+print(animalsList)
+
+
+
