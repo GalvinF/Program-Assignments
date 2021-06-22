af204721996fafc664a39936ace89bbd92c3ea98
GalvinF <78511747+GalvinF@users.noreply.github.com>
2021-06-22 11:32:24 -0700

    Simplify Code

--- Lab2.py
+++ Lab2.py
@@ -3,18 +3,12 @@ age = 35
 salary = 37.44
 plays_basketball = True
 jersey_number = "23"
-
-print(name)
-print(age)  
-print (salary)
-print(plays_basketball)
-print(jersey_number)
+print(name, age, salary, plays_basketball, jersey_number)
 
 car_name = "Subaru"
 x = 50
 my_first_name = "Galvin"
-
-print(car_name)
-print((type)x)
-print((type)my_first_name)
+print(car_name, x, my_first_name, car_name)
 type(car_name)
+type(50)
+type("Galvin")
