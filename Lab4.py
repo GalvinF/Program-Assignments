c12770703fd085c748aeb18ab4c509757f777862
GalvinF <78511747+GalvinF@users.noreply.github.com>
2021-06-29 12:29:52 -0700

    Added Comments & Fixed price difference function

--- Lab4.py
+++ Lab4.py
@@ -11,6 +11,7 @@ rectTwo = rectangleArea(7,9)
 print(rectOne + rectTwo)
 
 # Official lab starts now
+#Calculate total of two tiems
 groceries = {"Chicken": 1.59 , "Beef": 1.99 , "Cheese": 1.00 , "Milk": 2.50}
 shoes = {"Jordan 13": 1 , "Yeezy": 8 , "Foam Posite": 10 , "Air Max": 5 , "SB Dunk": 20}
 
@@ -26,12 +27,17 @@ def total_price(item1, item2):
 print("The price of Chicken and Cheese is: " + str(total_price("Chicken", "Cheese")))
 print("The price of Beef and Milk is: " + str(total_price("Beef", "Milk")))
 
+#Calculate difference of two items
 def price_difference(item1, item2):
     item1_price = groceries[item1]
     item2_price = groceries[item2]
-    price = (item1_price - item2_price)
 
-    return price
+    if item1_price > item2_price:
+        difference = item1_price - item2_price
+    else:
+        difference = item2_price - item1_price
+
+    return difference
 
 print("The difference between Chicken and Cheese is: " + str(price_difference("Chicken", "Cheese")))
-print("The difference between Milk and Beef is: " + str(price_difference("Milk", "Beef")))
+print("The difference between Beef and Milk is: " + str(price_difference("Beef", "Milk")))
